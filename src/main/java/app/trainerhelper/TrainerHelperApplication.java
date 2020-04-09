3a76dfd87929ee9a1d5f313d3f14ffc53b060f29
elefantaka <reniawiatr@gmail.com>
2020-04-09 22:35:00 +0200

    week1

---
+++ src/main/java/app/trainerhelper/TrainerHelperApplication.java
@@ -0,0 +1,13 @@
+package app.trainerhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/trainerhelper/TrainerHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package app.trainerhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
